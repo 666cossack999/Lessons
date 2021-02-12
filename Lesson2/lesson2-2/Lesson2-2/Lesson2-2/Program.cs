54bfe2416032b96aef33106742fe45f99b2654c4
Jackson <dmitrievei92@icloud.com>
2021-02-12 15:50:20 +0300

    lesson2-2

---
+++ Lesson2/lesson2-2/Lesson2-2/Lesson2-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lesson2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userMonth;
+            int n;
+
+            //Зацикливание программы пока пользователь не введёт данные от 1 до 12
+            do
+            {
+                //Запрашиваем у пользователя номер месяца и сохраняем в переменную userMonth
+                Console.Write("Введите номер месяца: ");
+                userMonth = Console.ReadLine();
+
+            } while (int.TryParse(userMonth, out n) == false);
+
+                //Проверка пользовательских данных. Номер месяца должен быть от 1 до 12
+                if (0 < n && n <= 12)
+                {
+                    //Создаём новую дату month с месяцем пользователя
+                    DateTime month = new DateTime(2021, n, 1);
+                    //Выводим месяц в буквенном формате
+                    Console.WriteLine(month.ToString("MMMM"));
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Введено неверное значение");
+                }
+
+            
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
