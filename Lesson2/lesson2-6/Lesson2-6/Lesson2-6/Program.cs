8308f98fe3c2119c4075fcadf29a52c93925bf21
Jackson <dmitrievei92@gmail.com>
2021-02-16 01:05:13 +0300

    Lesson2-6 finish

---
+++ Lesson2/lesson2-6/Lesson2-6/Lesson2-6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lesson2_6
+{
+    class Program
+    {
+        //Присваиваем дням недели битовые значения.
+        [Flags]
+        public enum DAYS : int
+        {
+            monday    = 0b0000001,
+            tuesday   = 0b0000010,
+            wednesday = 0b0000100,
+            thursday  = 0b0001000,
+            friday    = 0b0010000,
+            saturday  = 0b0100000,
+            sunday    = 0b1000000,
+        }
+        static void Main(string[] args)
+        {
+
+            //Присваиваем график первому офису
+            DAYS office1 = (DAYS)0b0011110;
+            //Присваиваем график второму офису
+            DAYS office2 = (DAYS)0b1111111;
+
+            //Выводим график работы 1го офиса
+            Console.WriteLine("Рабочие дни первого офиса: " + office1);
+            Console.WriteLine();
+            //Выводим график работы 2го офиса
+            Console.WriteLine("Рабочие дни второго офиса: " + office2);
+            Console.ReadLine();
+        }
+    }
+}
