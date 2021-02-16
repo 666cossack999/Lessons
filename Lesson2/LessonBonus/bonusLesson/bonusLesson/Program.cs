7efd824609509e86f000f28832cefb889589f9c3
Jackson <dmitrievei92@gmail.com>
2021-02-16 20:08:19 +0300

    BonusLesson finish

---
+++ Lesson2/LessonBonus/bonusLesson/bonusLesson/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace bonusLesson
+{
+    class Program
+    {   
+        static void LeapYear(int year)
+        {
+            if (year % 400 == 0)
+            {
+                Console.WriteLine("Год високосный!");
+            }
+            else if (year % 100 != 0 && year % 4 == 0 )
+            {
+                Console.WriteLine("Год високосный!");
+            }
+            else
+            {
+                Console.WriteLine("Год не високосный.");
+            }
+            
+            
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+
+                Console.WriteLine("Введите год, чтобы узнать високосный он или нет: ");
+                int year = int.Parse(Console.ReadLine());
+
+                LeapYear(year);
+            }
+        }
+    }
+}
