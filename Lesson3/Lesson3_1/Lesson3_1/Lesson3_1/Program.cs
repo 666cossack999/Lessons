5a71f137fe1eb78f6de9b58460a0fb9735d6ece0
Jackson <dmitrievei92@icloud.com>
2021-02-18 12:00:19 +0300

    Урок3_1 финал

---
+++ Lesson3/Lesson3_1/Lesson3_1/Lesson3_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаём двумерный массив 6,6
+            int[,] myMass = new int [6,6];
+            //Внешний цикл заполнения столбцов и перехода на новую строку
+            for (int i = 0; i < myMass.GetLength(0) ; i++)
+            {
+                //внутренний цикл заполнения строк
+                for (int j = 0; j < myMass.GetLength(1); j++)
+                {
+                    //условие для заполнения только диаганали
+                    if (i==j)
+                    {
+                        Console.Write(myMass[i, j] + " ");
+                    } else
+                    {
+                        Console.Write(" ");
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
