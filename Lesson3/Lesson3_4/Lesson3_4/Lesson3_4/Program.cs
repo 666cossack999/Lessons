5d309acb7e29edb08c4dd1135587d590ae9b64f0
Jackson <dmitrievei92@icloud.com>
2021-02-19 15:27:11 +0300

    Lesson3_4 update

---
+++ Lesson3/Lesson3_4/Lesson3_4/Lesson3_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] seaBattle = {
+            {'O','O','O','O','O','O','O','O','O','O'},
+            {'O','O','O','O','O','O','O','O','O','O'},
+            {'X','X','X','O','X','O','O','X','X','X'},
+            {'O','O','O','O','O','O','O','O','O','O'},
+            {'X','X','O','O','O','O','O','O','O','O'},
+            {'O','O','O','O','X','O','O','O','O','O'},
+            {'X','O','O','O','X','O','O','X','O','O'},
+            {'O','O','O','O','X','O','O','O','O','O'},
+            {'O','X','O','O','X','O','O','O','X','O'},
+            {'O','O','O','O','O','O','O','O','X','O'}
+            };
+
+            for (int i = 0; i < seaBattle.GetLength(0); i++)
+            {
+                for (int j = 0; j < seaBattle.GetLength(1); j++)
+                {
+                    Console.Write(seaBattle[i,j]);
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
