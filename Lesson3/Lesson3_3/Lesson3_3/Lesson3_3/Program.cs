a721c3826888675053d263bd8e0d0699da2ee8ae
Jackson <dmitrievei92@icloud.com>
2021-02-19 14:54:03 +0300

    Lesson3_3 update

---
+++ Lesson3/Lesson3_3/Lesson3_3/Lesson3_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любую строку: ");
+            string userString = Console.ReadLine();
+
+            for (int i = userString.Length - 1  ; i >= 0; i--)
+            {
+                Console.Write(userString[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
