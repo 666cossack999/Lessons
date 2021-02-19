187c9ba0e42fc219740d60cf724746d296516e52
Jackson <dmitrievei92@icloud.com>
2021-02-19 16:59:53 +0300

    Lesson3_5 update

---
+++ Lesson3/Lesson3_5/Lesson3_5/Lesson3_5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lesson3_5
+{
+    class Program
+    {
+
+        static string[] arrayCreate ()
+        {
+            Console.WriteLine("Введите кол-во элементов в массиве");
+            int Counts = int.Parse(Console.ReadLine());
+            string[] array = new string[Counts];
+
+            for (int i = 0; i < Counts; i++)
+            {
+                Console.WriteLine($"Введите элемент массива № {i}: ");
+                array[i] = Console.ReadLine();
+            }
+            return array;
+        }
+
+        static void printArray(string[] array)
+        {
+            Console.WriteLine("\n\nВаш массив: ");
+            for (int  i = 0;  i < array.Length;  i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine("\n\n");
+        }
+
+
+        static void Main(string[] args)
+        {
+           string[] myArray = arrayCreate();
+
+            Console.WriteLine("Введите число от -10 до 10, для смещения элементов массива: ");
+            int n = int.Parse(Console.ReadLine());
+
+            printArray(myArray);
+
+        }
+    }
+}
