f88ed02050c348bbc2871bd9ebec893bac19a8a2
Jackson <dmitrievei92@gmail.com>
2021-02-23 18:24:12 +0300

    All homework update

--- Lesson3/Lesson3_5/Lesson3_5/Lesson3_5/Program.cs
+++ Lesson3/Lesson3_5/Lesson3_5/Lesson3_5/Program.cs
@@ -4,7 +4,7 @@ namespace Lesson3_5
 {
     class Program
     {
-
+        //функция создания массива
         static string[] arrayCreate ()
         {
             Console.WriteLine("Введите кол-во элементов в массиве");
@@ -18,34 +18,90 @@ namespace Lesson3_5
             }
             return array;
         }
-
+        //функция вывода массива на консоль
         static void printArray(string[] array)
         {
-            Console.WriteLine("\n\nВаш массив: ");
+            
             for (int  i = 0;  i < array.Length;  i++)
             {
                 Console.Write(array[i] + " ");
             }
             Console.WriteLine("\n\n");
         }
-
-
-        static void Main(string[] args)
+        //Функция смещения элементов массива на n элементов. Возваращает смещённый массив
+        static string[] moveArray(string[] array, int n)
         {
-           string[] myArray = arrayCreate();
-
-            Console.WriteLine("Введите число от -10 до 10, для смещения элементов массива: ");
-            int n = int.Parse(Console.ReadLine());
-
-            printArray(myArray);
-
-            for (int i = 0; i < myArray.Length; i++)
+            //Если n > 0 - смещаем вправо
+            if (n > 0)
             {
-                string beta = myArray[i + n];
-                myArray[i+n] = myArray[i];
+                for (int i = 0; i < n; i++)
+                {
+                    string first = array[0];
+                    string last = array[array.Length - 1];
+                    for (int j = array.Length - 1; j > 0; j--)
+                    {
+                        if (j == 1)
+                        {
+                            array[j] = array[j - 1];
+                            array[1] = first;
+                        }
+                        else
+                        {
+                            array[j] = array[j - 1];
+                            array[0] = last;
+                        }
+                    }
+
+                }
+            } else {
+                //Если n < 0 - смещаем влево
+                for (int i = 0; i > n; i--)
+                {
+                    string first = array[0];
+                    string last = array[array.Length - 1];
+                    for (int j = 0; j < array.Length - 1; j++)
+                    {
+                        if (j == array.Length - 2)
+                        {
+                            array[j] = last;
+                            
+                        }
+                        else
+                    {
+                        array[j] = array[j + 1];
+                        array[array.Length - 1] = first;
+                    }
+                }
+
+                }
 
             }
 
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            //Зацикливаем программу для удобства
+            while (true)
+            { 
+                //создаём массив
+                string[] myArray = arrayCreate();
+
+                //Запрашиваем у пользователя число - на какое кол-во элементов сместить массив
+                Console.WriteLine("Введите число от -10 до 10, для смещения элементов массива: ");
+                int n = int.Parse(Console.ReadLine());
+
+                //Выводим первоначальный массив на консоль
+                Console.WriteLine("\n\nВаш массив: ");
+                printArray(myArray);
+                //смещаем массив
+                moveArray(myArray, n);
+                //выводим на консоль смещённый массив
+                Console.WriteLine("\n\nСмещённый массив: ");
+                printArray(myArray);
+            }
+            
         }
     }
 }
