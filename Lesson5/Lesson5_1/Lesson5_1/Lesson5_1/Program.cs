a18527da9b62ee3c0723a3cbf1f4ff9b4a17a3fc
Jackson <dmitrievei92@icloud.com>
2021-03-03 14:36:18 +0300

    Lesson5 update

---
+++ Lesson5/Lesson5_1/Lesson5_1/Lesson5_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "My.txt"; //имя файла
+            Console.WriteLine("Введите сообщение, которое хотите записать в файл: ");
+            string filetext = Console.ReadLine(); //считываем текст введённый с клавиатуры
+            File.WriteAllText(filename, filetext); //сохраняем текст с клавиатуры в файл
+
+
+        }
+    }
+}
