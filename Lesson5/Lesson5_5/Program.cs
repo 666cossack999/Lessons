afcc2c76c41f38d4eb529c6bd82cb71fa79f24a8
Jackson <dmitrievei92@gmail.com>
2021-03-03 21:44:58 +0300

    Lesson5_5 update

---
+++ Lesson5/Lesson5_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Lesson5_5
+{
+    class Program
+    {
+        static To_Do NewTitle()
+        {
+            var title = new To_Do();
+            title.Title = "Посмотреть вебинар";
+
+            return title;
+        }
+
+        static void Main(string[] args)
+        {
+            var title1 = NewTitle();
+            var title2 = NewTitle();
+
+            string tasks = JsonSerializer.Serialize(title1);            
+            tasks += JsonSerializer.Serialize(title2);
+            File.WriteAllText("To_Do.json", tasks);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lesson5/Lesson5_5/To-Do.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson5_5
+{
+    public class To_Do
+    {
+     
+        /// <summary>
+        /// Описание задачи
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Выполнена/Не выполнена задача (по умолчанию не выполнена)
+        /// </summary>
+        public bool IsDone { get; set; }
+
+        public To_Do()
+        {
+            IsDone = false;
+        }
+    }
+}
