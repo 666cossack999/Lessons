2435da89e1a4908997e801568df1fff800b65c35
Jackson <dmitrievei92@gmail.com>
2021-03-10 00:54:53 +0300

    Lesson6 Final

---
+++ Lesson6/Lesson6_4/employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson6_4
+{
+    class employee
+    {
+        public string FirstName;
+        public string LastName;
+        public string MiddleName;
+        public string Position;
+        public string Email;
+        public string Phone;
+        public int Wage;
+        public int Age;
+
+        public employee()
+        {
+        }
+
+        public employee(string lastName, string firstName, string middleName, string position, string email, string phone, int wage, int age)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+            MiddleName = middleName;
+            Position = position;
+            Email = email;
+            Phone = phone;
+            Wage = wage;
+            Age = age;
+        }
+
+
+
+        public string GetFullName()
+        {
+            return $"{LastName} {FirstName} {MiddleName}";
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Информация о сотруднике:");
+            Console.WriteLine($"Фамилия: {LastName}");
+            Console.WriteLine($"Имя: {FirstName}");
+            Console.WriteLine($"Отчество: {MiddleName}");
+            Console.WriteLine($"Должность: {Position}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Телефон: {Phone}");
+            Console.WriteLine($"Зарплата: {Wage}");
+            Console.WriteLine($"Возраст: {Age}");
+        }
+    }
+}
