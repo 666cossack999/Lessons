b78b8bae45c98ff3e60d4056180ffd45b6daf64e
Jackson <dmitrievei92@gmail.com>
2021-03-14 01:49:23 +0300

    Lesson7 update

--- Lesson7/Lesson7_1/Lesson7_1/Program.cs
+++ Lesson7/Lesson7_1/Lesson7_1/Program.cs
@@ -6,8 +6,9 @@ namespace Level_1.Lesson_7
 {
     class Cross
     {
-        static int SIZE_X = 3;
-        static int SIZE_Y = 3;
+        static int SIZE_X = 5;
+        static int SIZE_Y = 5;
+        static int WIN_SQUARE = 4;  //чило последовательности символов для победы
 
         static char[,] field = new char[SIZE_Y, SIZE_X];
 
@@ -99,43 +100,77 @@ namespace Level_1.Lesson_7
             SetSym(y, x, AI_DOT);
         }
 
+       
+        /// <summary>
+        /// Проверяет диагонали на победу
+        /// </summary>
+        /// <param name="sym">символ игрока</param>
+        /// <returns>true/false</returns>
+        private static bool CheckDiagonal(char sym,int offsetX, int offsetY)
+        {
+            bool toRight, toLeft;
+            toRight = true;
+            toLeft = true;
+            for (int i = offsetX; i < WIN_SQUARE; i++)
+            {
+                
+
+                    toRight &= field[i, i+offsetY] == sym;
+                    toLeft &= field[ i+offsetY, WIN_SQUARE - i - 1] == sym;
+                
+                
+                
+            }
+
+            return (toRight || toLeft);
+
+            
+        }
+        /// <summary>
+        /// Проверяем горизонтальные и вертикальные линии на победу
+        /// </summary>
+        /// <param name="sym">символ игрока</param>
+        /// <param name="offsetX">смещение по Х</param>
+        /// <param name="offsetY">смещение по Y</param>
+        /// <returns>true/false</returns>
+        private static bool CheckLanes(char sym,int offsetX, int offsetY)
+        {
+            bool columns, rows;
+            for (int col = offsetX; col < WIN_SQUARE+offsetX; col++)
+            {
+                columns = true;
+                rows = true;
+                
+                for (int row = offsetY; row < WIN_SQUARE+offsetY; row++)
+                {
+                    columns &= field[col, row] == sym;
+                    rows &= field[row, col] == sym;
+                }
+                //Если столбец или строка true - останавливаем дальнейшее выполнение
+                if (columns || rows) return true;
+                
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Делит игровое поле на игровые подполя размерностью WIN_SQUARE и делает проверку на победу в каждом подполе.
+        /// </summary>
+        /// <param name="sym">Символ игрока</param>
+        /// <returns>true/false</returns>
         private static bool CheckWin(char sym)
         {
-            if (field[0, 0] == sym && field[0, 1] == sym && field[0, 2] == sym)
+            
+            for (int col = 0; col <= (SIZE_X - WIN_SQUARE); col++)
             {
-                return true;
+                for (int row = 0; row <= (SIZE_X - WIN_SQUARE); row++)
+                {
+                    bool diag = CheckDiagonal(sym, col, row);
+                    bool lanes = CheckLanes(sym, col, row);
+                    if (diag || lanes) return true;
+                }
             }
-            if (field[1, 0] == sym && field[1, 1] == sym && field[1, 2] == sym)
-            {
-                return true;
-            }
-            if (field[2, 0] == sym && field[2, 1] == sym && field[2, 2] == sym)
-            {
-                return true;
-            }
-
-            if (field[0, 0] == sym && field[1, 0] == sym && field[2, 0] == sym)
-            {
-                return true;
-            }
-            if (field[0, 1] == sym && field[1, 1] == sym && field[2, 1] == sym)
-            {
-                return true;
-            }
-            if (field[0, 2] == sym && field[1, 2] == sym && field[2, 2] == sym)
-            {
-                return true;
-            }
-
-            if (field[0, 0] == sym && field[1, 1] == sym && field[2, 2] == sym)
-            {
-                return true;
-            }
-            if (field[2, 0] == sym && field[1, 1] == sym && field[0, 2] == sym)
-            {
-                return true;
-            }
-
+           
             return false;
         }
 
