cc013e2679d1e3f94d4ddfc68408e70f3f2cfa36
Jackson <dmitrievei92@icloud.com>
2021-03-17 12:18:29 +0300

    Lesson8 Final

---
+++ Lesson8/Lesson8/HelloLibrary/HelloClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloLibrary
+{
+    public class HelloClass
+    {
+        public static void SayHello()
+        {
+            var name = Environment.UserName;
+            Console.WriteLine($"Привет {name}! Как дела? ");
+        }
+    }
+}
---
+++ Lesson8/Lesson8/Lesson8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processes = Process.GetProcesses();
+            for (int i = 0; i < processes.Length; i++)
+            {
+                Console.Write($"{i}. ");
+                Console.WriteLine(processes[i].ProcessName);
+            }
+            Console.WriteLine("\nВведите номер процесса который хотите завершить: ");
+            int userID = int.Parse(Console.ReadLine());
+            processes[userID].Kill();
+        }   
+    }
+}
